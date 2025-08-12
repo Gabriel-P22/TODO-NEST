4fb9cf79fc4754f3da205120e5a24295d8473d6c
Gabriel <paschoalsoftwareengineer@gmail.com>
2025-08-12 16:02:09 -0300

    feature: add pipes

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,7 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
 import { Status } from '../enums/status.enum';
 
 export class CreateTodoDto {
+  @IsNotEmpty()
   id: string;
+
+  @IsNotEmpty()
   title: string;
   subTitle?: string;
   description: string;
